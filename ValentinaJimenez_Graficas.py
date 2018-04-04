cbc977f64827851212e0d6e2a34905dc261f5282
valauaca <37352292+valauaca@users.noreply.github.com>
2018-04-04 20:26:34 +0000

    Add files via upload

--- ValentinaJimenez_Graficas.py
+++ ValentinaJimenez_Graficas.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 datospython=np.loadtxt('times_python.csv')
 datosc=np.loadtxt('times_cpp.csv')
 
-#para separar las columnas n, tiempo 
+#para separar las columnas n, 
 
 x1=datospython[:,0]
 y1=datospython[:,1]
@@ -18,12 +18,12 @@ y2=datosc[:,1]
 plt.figure()
 a=plt.plot(x1,y1,color='k')
 b=plt.plot(x2,y2,color='b', marker='D')
-plt.title("Evoluacion temporal de la temperatura")
+plt.title("cpp vs python")
 plt.legend([a,b],["python","c++"])
-plt.xlabel("Tiempo (Anios)")
-plt.ylabel("Temperatura")
+plt.xlabel("N")
+plt.ylabel("tiempo")
 #plt.show()
-plt.savefig('temp_analisis.png')
+plt.savefig('cpp_vs_python.png')
 
 
 
